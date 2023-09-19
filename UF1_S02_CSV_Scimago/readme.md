92faf41dce905ead45f18fb53d60b45322c7dfeb
Miquel Amoros <mamoro10@xtec.cat>
2023-09-19 19:49:38 +0200

    Consultes Sessió 2

--- UF1_S02_CSV_Scimago/readme.md
+++ UF1_S02_CSV_Scimago/readme.md
@@ -8,5 +8,42 @@ L'objectiu d'aquest exercici és realitzar diverses consultes amb Python, i repa
 
 Recomanem organitzar el codi de l'exercici tal i com està en aquesta carpeta; posar cada consulta  en una funció (la q1 ja està resolta) i tenir per separat la classe que llegeix i tracta els fitxers CSV.
 
+## Consultes Sessió 2
 
+Q1 - How many entries are in scimago-medicine.csv?
+
+Q2 - Show the first 25 entries.
+
+Q3 - How many entries are from Spain? (Country = Spain)
+
+Hint Q3:
+```python
+for entry in entries:
+	entry['Country'] == 'Spain'
+```
+
+Q4 - Show all the journals (Type = journal) published in UK (Country = United Kingdom) with an 
+
+H-Index greater than 200, sorted by H-index (biggest H-Index first)
+
+Hint:
+https://stackoverflow.com/questions/72899/how-to-sort-a-list-of-dictionaries-by-a-value-of-the-dictionary-in-python
+
+Q5 - What types of scientific publications are in the file (paràmetre Type)?
+
+Hint; expected result:
+```python
+['journal', 'book series', 'conference and proceedings', 'trade journal']
+```
+
+Q6 - Count the number of types of each scientific publication.
+
+Q7 - Show all regions covered by all entries.
+
+Hint; expected result:
+```python
+{'Northern America', 'Middle East', 'Western Europe', 'Asiatic Region', 'Pacific Region', 'Latin America', 'Eastern Europe', 'Africa/Middle East', 'Africa'}
+```
+
+Q8 - Mean of H-index by region.
 
--- UF1_S02_CSV_Scimago/scimago-main-queries.py
+++ UF1_S02_CSV_Scimago/scimago-main-queries.py
@@ -2,6 +2,7 @@
 import file_utils
 # 3rd party imports
 import pprint 
+from operator import itemgetter
 
 # File name and path.
 csv_file_path: str = "scimago-medicine.csv"   
@@ -25,6 +26,48 @@ def q2_first_entries(entries: list[dict], num_first_entries: int) -> list[dict]:
     Output: The number of first entries defined in num_first_entries.'''
     return entries[0:num_first_entries]
 
+# -----------------------------------------------------------------------------
+# Q3 - How many entries are from Spain? (Country = Spain)
+# -----------------------------------------------------------------------------
+def q3_spanish_entries(entries: list[dict]) -> int:
+    '''Input: List of entries, number of first items.
+    Output: The number of Spanish entries.'''
+    numEntriesSpain: int = 0
+    for entry in entries:
+        if(entry['Country'] == 'Spain'):
+            numEntriesSpain+=1
+
+    return numEntriesSpain
+
+#Solució 32, funcional.
+def filterEntrySpain (entry:dict) -> bool:
+    '''Input: List of entries.
+    Output: True if the country of entry is Spain.'''      
+    return entry['Country'] == 'Spain'
+
+def q3_spanish_entries_v2(entries: list[dict]) -> int:
+    '''Input: List of entries, number of first items.
+    Output: The number of Spanish entries.'''
+    return len(list(filter(filterEntrySpain,entries)))
+
+# -----------------------------------------------------------------------------
+# Q4 - Show all the journals (Type = journal) published in UK 
+# (Country = United Kingdom) with an H-Index greater than 200, 
+# sorted by H-index (biggest H-Index first)
+# -----------------------------------------------------------------------------
+def q4(entries: list[dict]) -> list[dict]:
+    '''Input: List of entries.
+    Output: The number of .'''
+    filtered_entries: list[dict] = list(filter(filterUKJournalHIndex200,entries))
+    filtered_sorted_entries = sorted(filtered_entries, key=itemgetter('H index'),reverse=True)
+    return filtered_sorted_entries
+
+def filterUKJournalHIndex200(entry:dict) -> bool:
+    '''Input: List of entries.
+    Output: True if the country is UK, type journal, H-Index greater than 200.'''            
+    return entry['Country'] == 'United Kingdom' and entry['Type'] == 'journal' \
+        and int(entry['H index']) > 200                          
+
 
 # Main
 # -----------------------------------------------------------------------------
@@ -34,10 +77,20 @@ if __name__ == "__main__":
     # Test 
     # print(entries[0])
 
-    num_entries: list[dict] = q1_num_entries(entries)
-    print(f"There are {num_entries} entries.")
+    # num_entries: list[dict] = q1_num_entries(entries)
+    # print(f"There are {num_entries} entries.")
     
-    first_entries: list[dict] = q2_first_entries(entries,30)
-    # Prints the formatted representation of object followed by a newline. 
-    pprint.pp(first_entries[0])
+    # first_entries: list[dict] = q2_first_entries(entries,30)
+    # # Prints the formatted representation of object followed by a newline. 
+    # pprint.pp(first_entries[0])
+
+    num_spanish_entries: list[dict] = q3_spanish_entries_v2(entries)
+    # # Prints the formatted representation of object followed by a newline. 
+    print(f"There are {num_spanish_entries} entries from Spain.")
+
+    q4_entries: list[dict] = q4(entries)
+    # # Prints the formatted representation of object followed by a newline.
+    print('Query 4 results: journals from UK with H index greater than 200, sorted reverse') 
+    print(q4_entries[0:4])
+    print('---')
 # -----------------------------------------------------------------------------
