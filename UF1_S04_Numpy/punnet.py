508750b4abc6df7d73e57f906f20b1d786807100
Miquel Amorós <91978819+miquelamorosaldev@users.noreply.github.com>
2024-02-01 20:41:43 +0100

    Update punnet.py remove duplicate code

--- UF1_S04_Numpy/punnet.py
+++ UF1_S04_Numpy/punnet.py
@@ -53,30 +53,6 @@ print(f"6. {pregunta_6} \n   {respuesta_6}")
 print(f"7. {pregunta_7} \n   {respuesta_7}")
 print(f"8. {pregunta_8} \n   {respuesta_8}")
 
-# Preguntas y respuestas adicionales
-pregunta_5 = "¿Cuál es la probabilidad de que un gato tenga pelaje dominante (PpCC o PpCc)?"
-respuesta_5 = "La probabilidad de que un gato tenga pelaje dominante es 0.5."
-
-pregunta_6 = "Si los padres tienen genotipo PpCc y Ppcc, ¿cuál es la probabilidad de que un gato tenga pelaje recesivo y cola dominante (ppCC)?"
-respuesta_6 = "La probabilidad de que un gato tenga pelaje recesivo y cola dominante es 0.0625."
-
-pregunta_7 = "Supongamos que uno de los padres tiene genotipo PpCc y el otro ppCC. ¿Cuál es la probabilidad de que un gato tenga pelaje dominante y cola recesiva (Ppcc)?"
-respuesta_7 = "La probabilidad de que un gato tenga pelaje dominante y cola recesiva es 0.0625."
-
-pregunta_8 = "Si ambos padres tienen genotipo PpCc, ¿cuál es la probabilidad de que todos sus gatitos tengan pelaje dominante y cola dominante (PPCC)?"
-respuesta_8 = "La probabilidad de que todos sus gatitos tengan pelaje dominante y cola dominante es 0.0625."
-
-# Imprimir el cuadro dihíbrid y las probabilitats
-print("\nProbabilitats:")
-print(probabilitats)
-
-# Imprimir preguntas y respuestas adicionales
-print("\nPreguntas y Respuestas Adicionales:")
-print(f"5. {pregunta_5} \n   {respuesta_5}")
-print(f"6. {pregunta_6} \n   {respuesta_6}")
-print(f"7. {pregunta_7} \n   {respuesta_7}")
-print(f"8. {pregunta_8} \n   {respuesta_8}")
-
 
 # Configuració dels colors per als alels
 colors = {'PP': 'darkblue', 'Pp': 'mediumblue', 'pP': 'mediumblue', 'pp': 'lightblue',
@@ -107,7 +83,6 @@ plt.title('Quadre de Punnett - Gats (Pelatge i Cua)')
 # Eliminar els eixos
 ax.set_xticks([])
 ax.set_yticks([])
-
 
 #fig.add_legend('Age distribution by class')
 plt.savefig('plot-dist1.png')
